d361b187c69aa2fa34a8767de1155359980dd753
Adrian676767 <adriancervenka517@gmail.com>
2026-09-07 08:34:57 +0100

    Correct print statement in Nie.c
    Fix print statement and add newline at end of file.

---
+++ Nie.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void) {
+    int n;
+    printf("Zadaj cislo\n");
+    scanf("%d", &n);
+
+
+    for (int i = 0; i < 10; i++) {
+        print("Cislo i=%d\n", i);
+    }
+
+
+
+
+
+    return 0;
+}
