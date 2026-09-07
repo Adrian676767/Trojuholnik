935ce8a364ae79917aea2c39b28df7e6fe48c56c
Adrian676767 <adriancervenka517@gmail.com>
2026-09-07 08:34:40 +0100

    Add main function to check age and print messages

---
+++ main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(void) {
+    printf("Zadaj kolko mas rokov\n");
+    int vek;
+    scanf("%d", &vek);
+
+    if (vek > 18) {
+        printf("Mozes pit alkohol\n");
+    } else {
+        printf("Nemozes pit alkohol\n");
+     }
+
+
+    for (int i = 0; i < 10; i++) {
+        print("Cislo i=%d\n", i);
+    }
+
+
+
+
+         printf("Ahoj svet\n");
+
+    return 0; 
+}
